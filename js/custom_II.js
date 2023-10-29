27794e6830990e782adc387b570f17072525f734
Yeastic Hassan Zihan <85093810+zihan07@users.noreply.github.com>
2023-10-29 19:20:38 +0600

    Add files via upload

---
+++ js/custom_II.js
@@ -0,0 +1,172 @@
+(function($) {
+    "use strict";
+
+    var tpj = jQuery;
+    var revapi24;
+	
+	
+	// Preloader 
+	jQuery(window).on('load', function() {
+		jQuery("#status").fadeOut();
+		jQuery("#preloader").delay(350).fadeOut("slow");
+	});
+	
+	// on ready function
+    jQuery(document).ready(function($) {
+	
+	//show hide login form js
+	$('#search_button').on("click", function(e) {
+		$('#search_open').slideToggle();
+		e.stopPropagation(); 
+	});
+
+	$(document).on("click", function(e){
+		if(!(e.target.closest('#search_open'))){	
+			$("#search_open").slideUp();   		
+		}
+   });
+   
+   
+  
+	 
+		// Main Slider Animation
+
+        (function($) {
+
+            //Function to animate slider captions 
+            function doAnimations(elems) {
+                //Cache the animationend event in a variable
+                var animEndEv = 'webkitAnimationEnd animationend';
+
+                elems.each(function() {
+                    var $this = $(this),
+                        $animationType = $this.data('animation');
+                    $this.addClass($animationType).one(animEndEv, function() {
+                        $this.removeClass($animationType);
+                    });
+                });
+            }
+
+            //Variables on page load 
+            var $myCarousel = $('#carousel-example-generic'),
+                $firstAnimatingElems = $myCarousel.find('.carousel-item:first').find("[data-animation ^= 'animated']");
+
+            //Initialize carousel 
+            $myCarousel.carousel();
+
+            //Animate captions in first slide on page load 
+            doAnimations($firstAnimatingElems);
+
+            //Pause carousel  
+            $myCarousel.carousel('pause');
+
+
+            //Other slides to be animated on carousel slide event 
+            $myCarousel.on('click slide.bs.carousel', function(e) {
+                var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+                doAnimations($animatingElems);
+            });
+
+
+        })(jQuery);
+	
+	
+	//-------------------------------------------------------
+    // counter-section
+    //-------------------------------------------------------
+    $('.counter-section').on('inview', function(event, visible, visiblePartX, visiblePartY) {
+        if (visible) {
+            $(this).find('.timer').each(function () {
+                var $this = $(this);
+                $({ Counter: 0 }).animate({ Counter: $this.text() }, {
+                    duration: 2000,
+                    easing: 'swing',
+                    step: function () {
+                        $this.text(Math.ceil(this.Counter));
+                    }
+                });
+            });
+            $(this).off('inview');
+        }
+    });
+	
+	
+	
+		
+		/* 09. VENOBOX JS */
+            $('.venobox').venobox({
+                numeratio: true,
+                titleattr: 'data-title',
+                titlePosition: 'top',
+                spinner: 'wandering-cubes',
+                spinColor: '#007bff'
+            });
+			
+			
+				//----------- partners slider js -------------//
+	
+	$(document).ready(function() {
+              $('.partners_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:false,
+				smartSpeed: 1200,
+                responsiveClass: true,
+				navText : ['<i class="flaticon-left-arrow"></i>','<i class="flaticon-right-arrow"></i>'],
+                responsive: {
+					0: {
+                    items: 2,
+                    nav: true
+                  },
+                  600: {
+                    items: 2,
+                    nav: true
+                  },
+                  800: {
+                    items: 3,
+                    nav: true
+                  },
+                  1200: {
+                    items: 5,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+            })
+			
+			
+				$('.about_slider_wrapper .owl-carousel').owlCarousel({
+                loop: true,
+                margin: 10,
+				autoplay:true,
+				 smartSpeed: 1200,
+                responsiveClass: true,
+				navText : ['<i class="fa fa-angle-double-left" aria-hidden="true"></i>','<i class="fa fa-angle-double-right" aria-hidden="true"></i>'],
+                responsive: {
+                  0: {
+                    items: 1,
+                    nav: true
+                  },
+                  600: {
+                    items: 1,
+                    nav: true
+                  },
+                  1000: {
+                    items: 1,
+                    nav: true,
+                    loop: true,
+                    margin: 20
+                  }
+                }
+              })
+		
+		 /*--------------------------
+	nice Select active
+	---------------------------- */ 
+	$('select').niceSelect();
+	
+ });
+
+})(jQuery);	
